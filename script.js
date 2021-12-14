5500664b7baabaeafbf26dd4c917f5d941c8d4b0
Tim Hausweiler <timhausweiler@google.com>
2021-12-14 08:44:15 -0500

    navigating data structure of api

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ try {
   const url = `https://data.cityofnewyork.us/resource/gfqj-f768`;
   const res = await axios.get(url);
 
+  let randomIndex = Math.round(Math.random() * res.data.length);
+  let nextStory = res.data[randomIndex];
 } catch (error) {
   console.log(error);
 }
